b9a1474458bc1be23d9a787c089d590b883bbc71
devhindo <ahmed.hendawy@extremesolution.com>
2025-11-12 14:26:16 +0200

    Implement Apollo Server with GraphQL schema and mock database
    Signed-off-by: devhindo <ahmed.hendawy@extremesolution.com>

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import db from "./_db";
+
+// resolvers
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        authors() {
+            return db.authors;
+        }
+        ,
+        reviews() {
+            return db.reviews;
+        }
+    }
+}
+
+// server setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
+
